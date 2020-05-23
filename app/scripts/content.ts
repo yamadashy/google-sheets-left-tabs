514b7be8860d57d5f91318fcf6634b93be7253e3
Kazuki Yamada <koukun0120@gmail.com>
2020-05-23 23:40:05 +0900

    update: filter buttons

--- app/scripts/content.ts
+++ app/scripts/content.ts
@@ -1,5 +1,8 @@
+// modules
+import * as $ from 'jquery/dist/jquery.slim';
 import { waitElement } from './content/wait-element';
 
+// constants
 const DOCS_EDITOR_CONTAINER_ID = 'docs-editor-container';
 const DOCS_EDITOR_CONTAINER_BAR_CLASS = '.docs-sheet-container-bar';
 
@@ -10,13 +13,13 @@ class SheetTabsMover {
 
   addLeftTabs(): void {
     // wrapper
-    const $tabsWrapper = document.createElement('div');
-    $tabsWrapper.classList.add('sheets-left-tabs-wrapper');
+    const $tabsWrapper = $('<div>');
+    $tabsWrapper.addClass('sheets-left-tabs-wrapper');
 
     const $searchContainer = document.createElement('div');
     $searchContainer.classList.add('search-container');
-    const $buttonsContainer = document.createElement('div');
-    $buttonsContainer.classList.add('buttons-container');
+    const $buttonsContainer = $('<div>');
+    $buttonsContainer.addClass('buttons-container');
 
     // Add search
     const searchInput = document.createElement('input');
@@ -25,23 +28,33 @@ class SheetTabsMover {
     $searchContainer.appendChild(searchInput);
 
     // Add buttons
-    const $tabs = document.querySelectorAll('.docs-sheet-container-bar .docs-sheet-tab');
-    $tabs.forEach(($tab) => {
-      const leftTabButton = document.createElement('div');
-      leftTabButton.classList.add('left-tab-button');
-      leftTabButton.innerText = $tab.textContent;
+    const $tabs = $('.docs-sheet-container-bar .docs-sheet-tab');
+    $tabs.each((index, tab) => {
+      const $tab = $(tab);
+      const $leftTabButton = $('<div>');
+      $leftTabButton.addClass('left-tab-button');
+      $leftTabButton.text($tab.text());
 
-      if ($tab.classList.contains('docs-sheet-active-tab')) {
-        leftTabButton.classList.add('active');
+      if ($tab.hasClass('docs-sheet-active-tab')) {
+        $leftTabButton.addClass('active');
       }
 
-      $buttonsContainer.appendChild(leftTabButton);
+      // event
+      $leftTabButton.on('click', () => {
+        const $tab = $('.docs-sheet-container-bar .docs-sheet-tab').eq(index);
+        console.log($tab);
+        console.log(123);
+        $tab.trigger('mouseover');
+        $tab.trigger('click');
+      });
+
+      $buttonsContainer.append($leftTabButton);
     });
 
     // Add containers
-    $tabsWrapper.appendChild($searchContainer);
-    $tabsWrapper.appendChild($buttonsContainer);
-    document.getElementById(DOCS_EDITOR_CONTAINER_ID).prepend($tabsWrapper);
+    $tabsWrapper.append($searchContainer);
+    $tabsWrapper.append($buttonsContainer);
+    $('#' + DOCS_EDITOR_CONTAINER_ID).prepend($tabsWrapper);
 
     // // tmp events
     // var buttons = document.querySelectorAll(".buttons-container button");
@@ -59,19 +72,20 @@ class SheetTabsMover {
     //     // }).selectSheetByName(this.innerText);
     //   });
     // });
-    //
-    // document.querySelector(".search-container input").addEventListener("keyup", function() {
-    //   var inputValue = this.value;
-    //   var regInputValue = new RegExp(inputValue.split("").join(".*"), "i");
-    //
-    //   buttons.forEach(function(button) {
-    //     if (inputValue === "" || regInputValue.test(button.textContent)) {
-    //       button.classList.remove("hide");
-    //     } else {
-    //       button.classList.add("hide");
-    //     }
-    //   });
-    // });
+
+    var buttons = document.querySelectorAll(".buttons-container div.left-tab-button");
+    document.querySelector(".search-container input").addEventListener("keyup", function() {
+      var inputValue = this.value;
+      var regInputValue = new RegExp(inputValue.split("").join(".*"), "i");
+
+      buttons.forEach(function(button) {
+        if (inputValue === "" || regInputValue.test(button.textContent)) {
+          button.classList.remove("hide");
+        } else {
+          button.classList.add("hide");
+        }
+      });
+    });
 
   }
 }
